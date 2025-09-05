1ab99f196b5504615f81a3055f3a819df3025f90
danito <danimtorresh@gmail.com>
2025-09-04 19:46:52 -0600

    Creating structure and first file: bash

---
+++ checkFile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+pathToFile="/Users/danito/ProyectoPersonal/Proyectos/Profiling/data.txt"
+todays=$(date +'%d-%m-%Y %H:%M:%S')
+if [ -f $pathToFile ] ; then
+	echo "$todays Se ha encontrado el archivo. | tee -a checkFile.log profiling.log
+	if [ -s $pathToFile ] ; then
+		echo "$todays Iniciando el flujo de perfilado con python..." | tee -a checkFile.log profiling.log
+	else
+		echo "$todays ERROR: El archivo se encuentra vacío. No se puede iniciar el flujo de perfilado." | tee -a checkFile.log profiling.log
+	fi
+else
+	echo "$todays ERROR: No se ha encontrado el archivo en la ruta especificada" | tee -a checkFile.log profiling.log
+fi
+
